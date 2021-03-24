e6661a3a8c34229e9f24a02e6302998b6422d82e
Saurabh singh <66870178+saurabhjdsingh@users.noreply.github.com>
2021-03-24 09:05:49 +0530

    Update main.js

--- static/js/main.js
+++ static/js/main.js
@@ -91,3 +91,57 @@ $(function () {
         }
     });
 
+ let error = document.getElementById('validate');
+let label = document.getElementsByTagName("label");
+
+document.getElementById("name")
+  .addEventListener("keyup", function(e) {
+    if (e.keyCode === 13) {
+        e.preventDefault();
+      next("name", "email");
+    }
+  });
+
+  document.getElementById("email")
+  .addEventListener("keyup", function(e) {
+    if (e.keyCode === 13) {
+        e.preventDefault();
+        next('email','phone');
+    }
+  });
+
+  document.getElementById("phone")
+  .addEventListener("keyup", function(e) {
+    if (e.keyCode === 13) {
+        e.preventDefault();
+        next('phone','age');
+    }
+  });
+
+  document.getElementById("location")
+  .addEventListener("keyup", function(e) {
+    if (e.keyCode === 13) {
+        e.preventDefault();
+      next('location','message');
+    }
+  });
+ 
+
+function next(from, to) {
+    error.innerHTML = "";
+    let value = document.getElementById(from).children[1].value;
+    if(!value || value === "") {
+        error.innerHTML = "Please enter a valid value";
+    }
+    else {
+        error.innerHTML = "";
+        document.getElementById(from).classList.remove('is-visible');
+        document.getElementById(to).classList.add('is-visible');
+    }
+}
+
+function previous(from , to) {
+    error.innerHTML = "";
+    document.getElementById(from).classList.remove('is-visible');
+    document.getElementById(to).classList.add('is-visible');
+}
